d76855437c7c7f664733fb4a4eb1439e4195ad12
Neil.Zhou <sifutian@163.com>
2016-04-26 23:51:54 +0800

    add first page

---
+++ webroot/js/wedding.js
@@ -0,0 +1,19 @@
+function mp3_play(mp3_url){
+    var qt_mp3_url = "http://mp3.123hl.cn/wodemingzijiaoyilian.m4a";
+    if (mp3_url) {
+        $('#playbox audio').get(0).pause();
+        $('#playbox audio').get(0).setAttribute('src', mp3_url);
+        //$('#playbox audio').get(0).setAttribute('loop', false);
+        $('#playbox audio').get(0).load();
+        $('#playbox audio').get(0).play();
+    }else{
+        $('#playbox audio').get(0).setAttribute('src', qt_mp3_url);
+        if ($('#playbox').hasClass('on')){
+            $('#playbox').attr('class','');
+            $('#playbox audio').get(0).pause();
+        }else{
+            $('#playbox').attr('class','on');
+            $('#playbox audio').get(0).play();
+        }
+    }
+}
