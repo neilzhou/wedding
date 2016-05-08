99089eb9582ad34aea4304a276df664b1791ea4f
Neil.Zhou <sifutian@163.com>
2016-05-08 12:33:53 +0800

    add photos for wedding

--- webroot/js/wedding.js
+++ webroot/js/wedding.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
   //$('section').height($(window).height());
-var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'vertical',
-    loop: true,
-  })        
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'vertical',
+        mousewheelControl: true,
+        onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+            swiperAnimateCache(swiper); //隐藏动画元素 
+            swiperAnimate(swiper); //初始化完成开始动画
+        }, 
+        onSlideChangeEnd: function(swiper){ 
+            swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+        } 
+    })        
 });
 function mp3_play(mp3_url){
     var qt_mp3_url = "http://mp3.123hl.cn/wodemingzijiaoyilian.m4a";
